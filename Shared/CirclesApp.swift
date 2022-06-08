1bfda0e5e3af6be4ab8e4db6e9cec182dd721012
Joseph DeWeese <rangerboy901@icloud.com>
2022-06-08 17:53:01 -0500

    Initial Commit

---
+++ Shared/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Shared
+//
+//  Created by Joseph DeWeese on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
